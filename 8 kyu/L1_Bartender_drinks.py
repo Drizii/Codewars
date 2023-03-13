3abdb8c106352653b50862281f910c3f2eef509b
Drizii <wojtas1478@gmail.com>
2023-03-13 16:20:28 +0100

    L1: Bartender, drinks!

--- 8 kyu/L1_Bartender_drinks.py
+++ 8 kyu/L1_Bartender_drinks.py
@@ -1,13 +1,9 @@
 def get_drink_by_profession(param):
-    my_dict = {
+    return {
         "jabroni": "Patron Tequila",
         "school counselor": "Anything with Alcohol",
         "programmer": "Hipster Craft Beer",
         "bike gang member": "Moonshine",
         "politician": "Your tax dollars",
         "rapper": "Cristal",
-    }
-    for key, value in my_dict.items():
-        if key == param.lower():
-            return value
-    return "Beer"
+    }.get(param.lower(), "Beer")
